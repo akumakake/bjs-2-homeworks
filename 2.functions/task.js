2e800ec4690a77beb32b9d152618f82622c204b6
Test <dharzhevskij@gmail.com>
2025-09-26 18:15:46 +0300

    1 задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,27 @@
 function getArrayParams(...arr) {
+  if (arr.length === 0) {
+  return { min: 0, max: 0, avg: 0 };
+  }
 
-  return { min: min, max: max, avg: avg };
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  const avg = Numder((sum / arr.length).toFixed(2));
+
+  return { min, max, avg };
 }
 
 function summElementsWorker(...arr) {
