0321c21ae171f267a698124b9fa65267de919112
Test <dharzhevskij@gmail.com>
2025-09-26 20:22:11 +0300

    добавил 2 задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -25,15 +25,34 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0){
+    return 0;
+  }
+  return arr.reduce((sum, current) => sum + current, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  return max-min;
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
