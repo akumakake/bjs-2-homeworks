7e927ba0efe3843e2e3370d45f7f71a94d084353
Test <dharzhevskij@gmail.com>
2025-09-27 09:38:06 +0300

    добавил 2 задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -8,4 +8,13 @@ function compareArrays(arr1, arr2) {
 
 function getUsersNamesInAgeRange(users, gender) {
   
+    const filteredUsers = users.filter(user => user.gender === gender);
+    
+    if (filteredUsers.length === 0) {
+        return 0;
+    }
+    
+    const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    
+    return totalAge / filteredUsers.length;
 }
