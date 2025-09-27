3d3d02d62cb616c07ab313c899bc1f87a34b05e0
Test <dharzhevskij@gmail.com>
2025-09-27 09:30:09 +0300

    добавил 1 задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
